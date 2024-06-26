e44642fda5e2f5af0b8d62768f6ba3b1125b79e1
20k <icedshot@gmail.com>
2024-06-26 04:38:00 +0100

    work

--- code/wormholes/accretion_disk.cpp
+++ code/wormholes/accretion_disk.cpp
@@ -91,7 +91,7 @@ accretion_disk make_accretion_disk_kerr(float mass, float a)
 
     double Msol_natural = (Msol * cst_G) / (cst_C*cst_C);
 
-    double m_star = mass / (3 * Msol_natural);
+    double m_star = mass / Msol_natural;
 
     double eddington_kg_ps = eddington_limit_kg_s(mass);
 
@@ -113,7 +113,7 @@ accretion_disk make_accretion_disk_kerr(float mass, float a)
     double outer_boundary = 2 * mass * 50;
 
     ///0 = plunge, 1 = edge, 2 = inner, 3 = middle, 4 = outer
-    int region = 0;
+    int region = 2;
 
     int max_steps = 1000;
 
@@ -139,10 +139,7 @@ accretion_disk make_accretion_disk_kerr(float mass, float a)
 
     for(int steps = 0; steps < max_steps; steps++)
     {
-        double r = mix(horizon, outer_boundary, steps / (double)max_steps);
-
-        if(region == 0 && r >= isco)
-            region = 1;
+        double r = mix(isco, outer_boundary, steps / (double)max_steps);
 
         double x = sqrt(r/mass);
 
@@ -151,6 +148,7 @@ accretion_disk make_accretion_disk_kerr(float mass, float a)
         ///so, when r/m > 2, we're outside the event horizon
         ///because rs = 2m
 
+        float r_star = r / mass;
 
         float x_pow_m2 = mass/r;
         float x_pow_m4 = cpow(mass/r, 2.);
@@ -161,58 +159,36 @@ accretion_disk make_accretion_disk_kerr(float mass, float a)
         double A = 1 + assq * x_pow_m4 + 2 * assq * cpow(x, -6.);
         double B = 1 + a_star * cpow(x, -3.);
         double C = 1 - 3 * x_pow_m2 + 2 * assq * cpow(x, -3.);
-        double D = 1 - 2 * (mass/r) + assq * cpow(x, -4.);
-        //double D = 1 - 2 * cpow(x, -2.) + assq * cpow(x, -4.);
+        double D = 1 - 2 * x_pow_m2 + assq * cpow(x, -4.);
         double E = 1 + 4 * assq * x_pow_m4 - 4 * assq * cpow(x,-6.) + 3 * cpow(a_star, 4.) * cpow(x, -8.);
         double F = 1 - 2 * a_star * cpow(x, -3.) + assq * x_pow_m4;
         double G = 1 - 2 * x_pow_m2 + a_star * cpow(x, -3.);
-        double H = 1 - 2 * x_pow_m2 + 2 * a_star * cpow(x, -2.) * cpow(x0, -1.) * cpow(F0, -1.) * G0;
-        double I = A - 2 * a_star * cpow(x, -6.) * x0 * F0 * cpow(G0, -1.);
-        double O = H * cpow(I, -1.);
-        double J = O - cpow(x, -2.) * cpow(I, -1.) * (1 - a_star * cpow(x0, -1.) * cpow(F0, -1.) * G0 + assq * cpow(x, -2.) * H * cpow(I, -1.) * (1 + 3 * cpow(x, -2.) - 3 * cpow(a_star, -1.) * cpow(x, -2.) * x0 * F0 * cpow(G0, -1.)));
-        double K = fabs(A * J * cpow(1 - cpow(x, -4.) * cpow(A, 2.) * cpow(D, -1.) * cpow(x0 * F0 * cpow(G0, -1.) * O - 2 * a_star * cpow(x, -2.) * cpow(A, -1.), 2.), -1.));
         double Q = B * cpow(C, -1/2.) * (1/x) * (x - x0 - (3/2.) * a_star * log(x/x0)
                                                 - (3 * cpow(x1 - a_star, 2.) / (x1 * (x1 - x2) * (x1 - x3))) * log((x - x1) / (x0 - x1))
                                                 - (3 * cpow(x2 - a_star, 2.) / (x2 * (x2 - x1) * (x2 - x3))) * log((x - x2) / (x0 - x2))
                                                 - (3 * cpow(x3 - a_star, 2.) / (x3 * (x3 - x1) * (x3 - x2))) * log((x - x3) / (x0 - x3))
                                                 );
-        double R = cpow(F, 2.) * cpow(C, -1.) - assq * cpow(x, -2.) * (G * cpow(C, -1./2.) - 1);
-        double S = cpow(A, 2.) * cpow(B, -2.) * C * cpow(D, -1.) * R;
-        double V = cpow(D, -1.) * (1 + cpow(x, -4.) * (assq - cpow(x0, 2.) * cpow(F0, 2.) * cpow(G0, -2.)) + 2 * cpow(x, -6.) * (a_star - x0 * F0 * cpow(G0, -1.)));
+        ///ok, this is B/(1-B)
+        double p_gas_p_rad = 0;
 
-        double Phi = Q + (0.02) * (cpow(alpha, 9/8.) * cpow(m_star, -3/8.) * cpow(mdot_star, 1/4.) * cpow(x, -1.) * B * cpow(C, -1/2.) * (cpow(x0, 9/8.) * cpow(C0, -5/8.) * G0 * cpow(V0, 1/2.)));
+        if(region == 4)
+            p_gas_p_rad = 3 * cpow(alpha, -1/10.) * cpow(m_star, -1/10.) * cpow(mdot_star, -7/20.) * cpow(r_star, 3/8.) * cpow(A, -11/20.) * cpow(B, 9/10.) * cpow(D, 7/40.) * cpow(E, 11/40.) * cpow(Q, -7/20.);
 
-        double p_gas_p_rad = (5 * cpow(10., -5.)) * (cpow(alpha, -1/4.) * cpow(m_star, 7/4.) * cpow(mdot_star, -2.)) * cpow(x, 21/4.) * cpow(A, -5/2.) * cpow(B, 9/2.) * D * cpow(S, 5/4.) * cpow(Phi, -2.);
+        if(region == 3)
+            p_gas_p_rad = 7 * cpow(10., -3.) * cpow(alpha, -1/10.) * cpow(m_star, -1/10.) * cpow(mdot_star, -4/5.) * cpow(r_star, 21/20.) * cpow(A, -1.) * cpow(B, 9/5.) * cpow(D, 2/5.) * cpow(E, 1/2.) * cpow(Q, -4/5.);
 
-        double v_star_inner = (cpow(C0, -1.) * cpow(G0, 2.) * V - 1) + 7 * cpow(10., -3.) * cpow(alpha, 1/4.) * cpow(m_star, -3/4.) * cpow(mdot_star, 1/2.) * cpow(x0, -7/4.) * cpow(C0, -5/4.) * cpow(D0, -1.) * cpow(G0, 2.) * V;
-
-        //v_star_inner = fabs(v_star_inner);
-
-        /*std::cout << "Test " << (1/(x*x*x*x)) * -x0*x0 * F0*F0 * 1/(G0*G0) << std::endl;
-        std::cout << "Test2 " << 2 * cpow(x, -6.) * -x0 * F0 / G0 << std::endl;
-        std::cout << "D " << D << std::endl;
-
-        std::cout << "A " << A << " B " << B << " C " << C << " D " << D << " E " << E << std::endl;
-        std::cout << "V " << V << std::endl;*/
-
-        //std::cout << "VTest " << cpow(D, -1.) << std::endl;
-
-        //std::cout << "DOM " << (cpow(C0, -1.) * cpow(G0, 2.) * V - 1) << std::endl;
-
-        double v_star = sqrt(v_star_inner);
+        if(region == 2)
+            p_gas_p_rad = 4 * cpow(10., -6.) * cpow(alpha, -1/4.) * cpow(m_star, -1/4.) * cpow(mdot_star, -2.) * cpow(r_star, 21/8.) * cpow(A, -5/2.) * cpow(B, 9/2.) * D * cpow(E, 5/4.) * cpow(Q, -2.);
 
         ///in the edge region, gas pressure dominates over radiation pressure
         ///in the inner region, gas pressure is less than radiation pressure
         ///in the middle region, gas pressure is greater than radiation pressure
-        if(region == 1 && p_gas_p_rad < 1)
-            region = 2;
-
         if(region == 2 && p_gas_p_rad > 1)
             region = 3;
 
         //in the outer region opacity is free-free
         //in the middle region, opacity is electron scattering
-        double Tff_Tes = (0.6 * cpow(10.,-5.)) * (m_star * cpow(mdot_star, -1.)) * cpow(x, 3.) * cpow(A, -1.) * cpow(B, 2.) * cpow(D, 1/2.) * cpow(S, 1/2.) * cpow(Phi, -1.);
+        double Tff_Tes = (2 * cpow(10., -6.)) * (cpow(mdot_star, -1.)) * cpow(r_star, 3/2.) * cpow(A, -1.) * cpow(B, 2.) * cpow(D, 1/2.) * cpow(E, 1/2.) * cpow(Q, -1.);
 
         //std::cout << "Tff " << Tff_Tes << std::endl;
 
@@ -222,31 +198,30 @@ accretion_disk make_accretion_disk_kerr(float mass, float a)
         double surface_flux = 0;
         double temperature = 0;
         double density = 0;
-        double surface_density = 0;
-
-        if(region == 0)
-        {
-            //erg/cm^2 sec
-            surface_flux = 2 * cpow(10., 18.) * (cpow(alpha, 4/3.) * cpow(m_star, -3.) * cpow(mdot_star, 5/3.)) * cpow(x, -26/3.) * cpow(x0, 4/3.) * cpow(D, -5/6.) * cpow(K, 4/3.) * cpow(F0, 4/3.) * cpow(G0, -4/3.) * cpow(v_star, -5/3.);
-            surface_density = 1 * cpow(m_star, -1.) * mdot_star * cpow(x, -2.) * cpow(D, -1/2.) * cpow(v_star, -1.);
-        }
+        //double surface_density = 0;
 
         if(region == 1 || region == 3)
         {
-            surface_flux = 0.6 * cpow(10., 26.) * (cpow(m_star, -2.) * mdot_star) * cpow(x, -6.) * cpow(B, -1.) * cpow(C,-1/2.) * Phi;
-            surface_density = 5 * cpow(10., 4.) * cpow(alpha, -4/5.) * cpow(m_star, -2/5.) * cpow(mdot_star, 3/5.) * cpow(x, -6/5.) * cpow(B, -4/5.) * cpow(C, -1/2.) * cpow(D, -4/5.) * cpow(Phi, 3/5.);
+            surface_flux = 7 * cpow(10., 26.) * cpow(m_star, -1.) * mdot_star * cpow(r_star, -3.) * cpow(B, -1.) * cpow(C, -1/2.) * Q;
+            //surface_flux = 0.6 * cpow(10., 26.) * (cpow(m_star, -2.) * mdot_star) * cpow(x, -6.) * cpow(B, -1.) * cpow(C,-1/2.) * Phi;
+            //surface_density = 5 * cpow(10., 4.) * cpow(alpha, -4/5.) * cpow(m_star, -2/5.) * cpow(mdot_star, 3/5.) * cpow(x, -6/5.) * cpow(B, -4/5.) * cpow(C, -1/2.) * cpow(D, -4/5.) * cpow(Phi, 3/5.);
+
         }
 
         if(region == 2)
         {
-            surface_flux = 0.6 * cpow(10., 26.) * (cpow(m_star, -2.) * mdot_star) * cpow(x, -6.) * cpow(B, -1.) * cpow(C,-1/2.) * Phi;
-            surface_density = 20 * cpow(alpha, -1.) * m_star * cpow(mdot_star, -1.) * cpow(x, 3.) * cpow(A, -2.) * cpow(B, 3.) * cpow(C, 1/2.) * S * cpow(Phi, -1.);
+            surface_flux = 7 * cpow(10., 26.) * cpow(m_star, -1.) * mdot_star * cpow(r_star, -3.) * cpow(B, -1.) * cpow(C, -1/2.) * Q;
+            //surface_density = 5 * cpow(alpha, -1.) * cpow(mdot_star, -1.) * cpow(r_star, 3/2.) * cpow(A, -2.) * cpow(B,3.) * cpow(C, 1/2.) * E * cpow(Q, -1.);
+
+            //surface_flux = 0.6 * cpow(10., 26.) * (cpow(m_star, -2.) * mdot_star) * cpow(x, -6.) * cpow(B, -1.) * cpow(C,-1/2.) * Phi;
+            //surface_density = 20 * cpow(alpha, -1.) * m_star * cpow(mdot_star, -1.) * cpow(x, 3.) * cpow(A, -2.) * cpow(B, 3.) * cpow(C, 1/2.) * S * cpow(Phi, -1.);
         }
 
         if(region == 4)
         {
-            surface_flux = 0.6 * cpow(10., 26.) * (cpow(m_star, -2.) * mdot_star) * cpow(x, -6.) * cpow(B, -1.) * cpow(C,-1/2.) * Phi;
-            surface_density = 2 * cpow(10., 5.) * cpow(alpha, -4/5.) * cpow(m_star, -1/2.) * cpow(mdot_star, 7/10.) * cpow(x, -3/2.) * cpow(A, 1/10.) * cpow(B, -4/5.) * cpow(C, 1/2.) * cpow(D, -17/20.) * cpow(S, -1/20.) * cpow(Phi, 7/10.);
+            surface_flux = 7 * cpow(10., 26.) * cpow(m_star, -1.) * mdot_star * cpow(r_star, -3.) * cpow(B, -1.) * cpow(C, -1/2.) * Q;
+            //surface_flux = 0.6 * cpow(10., 26.) * (cpow(m_star, -2.) * mdot_star) * cpow(x, -6.) * cpow(B, -1.) * cpow(C,-1/2.) * Phi;
+            //surface_density = 2 * cpow(10., 5.) * cpow(alpha, -4/5.) * cpow(m_star, -1/2.) * cpow(mdot_star, 7/10.) * cpow(x, -3/2.) * cpow(A, 1/10.) * cpow(B, -4/5.) * cpow(C, 1/2.) * cpow(D, -17/20.) * cpow(S, -1/20.) * cpow(Phi, 7/10.);
         }
 
         if(!std::isnan(surface_flux))
@@ -326,6 +301,8 @@ accretion_disk make_accretion_disk_kerr(float mass, float a)
     accretion_disk disk;
 
     ///we use units of c=g=1
+
+    assert(false);
 
     return disk;
 }
