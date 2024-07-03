81f92c0f8a00fd05ddb0dba88ab2501752a38e36
20k <icedshot@gmail.com>
2024-07-03 13:27:56 +0100

    basic window rendering

--- code/NR1/main.cpp
+++ code/NR1/main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
-
-using namespace std;
+#include <toolkit/render_window.hpp>
+#include <imgui/misc/freetype/imgui_freetype.h>
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    render_settings sett;
+    sett.width = 1280;
+    sett.height = 720;
+    sett.opencl = true;
+    sett.no_double_buffer = false;
+    sett.is_srgb = true;
+    sett.no_decoration = false;
+    sett.viewports = false;
+
+    render_window win(sett, "Geodesics");
+
+    assert(win.clctx);
+
+    ImFontAtlas* atlas = ImGui::GetIO().Fonts;
+    atlas->FontBuilderFlags = ImGuiFreeTypeBuilderFlags_LCD | ImGuiFreeTypeBuilderFlags_FILTER_DEFAULT | ImGuiFreeTypeBuilderFlags_LoadColor;
+
+    ImFontConfig font_cfg;
+    font_cfg.GlyphExtraSpacing = ImVec2(0, 0);
+    font_cfg.FontBuilderFlags = ImGuiFreeTypeBuilderFlags_LCD | ImGuiFreeTypeBuilderFlags_FILTER_DEFAULT | ImGuiFreeTypeBuilderFlags_LoadColor;
+
+    ImGuiIO& io = ImGui::GetIO();
+
+    io.Fonts->Clear();
+    io.Fonts->AddFontFromFileTTF("VeraMono.ttf", 14, &font_cfg);
+
+    while(!win.should_close())
+    {
+        win.poll();
+
+        ImGui::Begin("Hi");
+
+        ImGui::End();
+
+        win.display();
+    }
 }
