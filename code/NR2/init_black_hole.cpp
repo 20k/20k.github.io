c417a3a2e675d5cc3a99d0dba3094a4e99e038fd
20k <icedshot@gmail.com>
2024-08-05 00:46:28 +0100

    cleanup

--- code/NR2/init_black_hole.cpp
+++ code/NR2/init_black_hole.cpp
@@ -22,20 +22,16 @@ tensor<valuef, 3, 3> get_aIJ(v3f world_pos, v3f bh_pos, v3f angular_momentum, v3
     {
         for(int j=0; j < 3; j++)
         {
-            tensor<valuef, 3> momentum_tensor = {momentum.x(), momentum.y(), momentum.z()};
+            valuef r = (world_pos - bh_pos).length();
 
-            tensor<valuef, 3> vri = {bh_pos.x(), bh_pos.y(), bh_pos.z()};
+            r = max(r, valuef(1e-6f));
 
-            valuef ra = (world_pos - vri).length();
+            tensor<valuef, 3> n = (world_pos - bh_pos) / r;
 
-            ra = max(ra, valuef(1e-6f));
+            tensor<valuef, 3> momentum_lo = flat.lower(momentum);
+            tensor<valuef, 3> n_lo = flat.lower(n);
 
-            tensor<valuef, 3> nia = (world_pos - vri) / ra;
-
-            tensor<valuef, 3> momentum_lower = lower_index(momentum_tensor, flat, 0);
-            tensor<valuef, 3> nia_lower = lower_index(tensor<valuef, 3>{nia.x(), nia.y(), nia.z()}, flat, 0);
-
-            bcAij[i, j] += (3 / (2.f * ra * ra)) * (momentum_lower[i] * nia_lower[j] + momentum_lower[j] * nia_lower[i] - (flat[i, j] - nia_lower[i] * nia_lower[j]) * sum_multiply(momentum_tensor, nia_lower));
+            bcAij[i, j] += (3 / (2.f * r * r)) * (momentum_lo[i] * n_lo[j] + momentum_lo[j] * n_lo[i] - (flat[i, j] - n_lo[i] * n_lo[j]) * sum_multiply(momentum, n_lo));
 
             ///spin
             valuef s1 = 0;
@@ -45,12 +41,12 @@ tensor<valuef, 3, 3> get_aIJ(v3f world_pos, v3f bh_pos, v3f angular_momentum, v3
             {
                 for(int l=0; l < 3; l++)
                 {
-                    s1 += eijk[k, i, l] * angular_momentum[l] * nia[k] * nia_lower[j];
-                    s2 += eijk[k, j, l] * angular_momentum[l] * nia[k] * nia_lower[i];
+                    s1 += eijk[k, i, l] * angular_momentum[l] * n[k] * n_lo[j];
+                    s2 += eijk[k, j, l] * angular_momentum[l] * n[k] * n_lo[i];
                 }
             }
 
-            bcAij[i, j] += (3 / (ra*ra*ra)) * (s1 + s2);
+            bcAij[i, j] += (3 / (r*r*r)) * (s1 + s2);
         }
     }
 
