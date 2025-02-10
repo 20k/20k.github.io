a688806a637d114d2d07b1424aba37125e5700cf
20k <icedshot@gmail.com>
2025-02-10 19:58:35 +0000

    start of solving

--- code/NR4/init_neutron_star.hpp
+++ code/NR4/init_neutron_star.hpp
@@ -5,18 +5,17 @@
 #include <vector>
 #include "tov.hpp"
 
-///end goal: calculate ppw2p, provide a discretisation facility
+///end goal: calculate conformal ppw2p and conformal aij_aij
 ///take in a tov solution
-
 namespace neutron_star
 {
-    struct neutron_star_solution
+    struct solution
     {
-        std::vector<double> ph;
-        std::vector<tensor<double, 3, 3>> Aij;
+        std::vector<double> ph_cfl;
+        std::vector<tensor<double, 3, 3>> AIJ_cfl;
     };
 
-    neutron_star_solution solve(const tov::integration_solution& sol);
+    solution solve(const tov::integration_solution& sol);
 }
 
 #endif // INIT_NEUTRON_STAR_HPP_INCLUDED
