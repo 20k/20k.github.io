44ec08988dc95e0d63e39680ba044cb03dd0eb97
20k <icedshot@gmail.com>
2025-02-11 00:08:38 +0000

    vector sources

--- code/NR4/init_neutron_star.hpp
+++ code/NR4/init_neutron_star.hpp
@@ -8,28 +8,40 @@
 
 ///end goal: calculate conformal ppw2p and conformal aij_aij
 ///take in a tov solution
+///todo: unify all the params structs
 namespace neutron_star
 {
+    struct params
+    {
+        tensor<float, 3> position;
+        tensor<float, 3> linear_momentum;
+        tensor<float, 3> angular_momentum;
+    };
+
     ///so, I'm going to crack all the constants in here, then perform the discretisation on the GPU
     struct solution
     {
         std::vector<double> mu_cfl;
+        std::vector<double> mu_h_cfl;
         std::vector<double> pressure_cfl;
+
+        ///todo: ph_cfl? it requires the neutron star params, spin and linear momentum
 
         std::vector<double> N;
         std::vector<double> Q;
         std::vector<double> C;
     };
 
-    solution solve(const tov::integration_solution& sol);
+    solution solve(const params& phys_params, const tov::integration_solution& sol);
 
     struct discretised_solution
     {
         cl::buffer mu_cfl;
+        cl::buffer mu_h_cfl;
         cl::buffer pressure_cfl;
         std::array<cl::buffer, 6> AIJ_cfl;
 
-        discretised_solution(cl::context& ctx) : mu_cfl(ctx), pressure_cfl(ctx), AIJ_cfl{ctx, ctx, ctx, ctx, ctx, ctx}{}
+        discretised_solution(cl::context& ctx) : mu_cfl(ctx), mu_h_cfl(ctx), pressure_cfl(ctx), AIJ_cfl{ctx, ctx, ctx, ctx, ctx, ctx}{}
     };
 
     void add_to_solution(cl::context& ctx, cl::command_queue& cqueue, discretised_solution& dsol, const solution& nsol, const tov::integration_solution& tsol);
