919c925acc32826ab2723791abb81a9e01dccbb3
20k <icedshot@gmail.com>
2025-02-12 09:39:29 +0000

    more plugin work

--- code/NR4/bssn.hpp
+++ code/NR4/bssn.hpp
@@ -6,30 +6,7 @@
 #include "../common/value2.hpp"
 #include "../common/single_source.hpp"
 #include <toolkit/opencl.hpp>
-
-using derivative_t = value<float16>;
-using valuef = value<float>;
-using valued = value<double>;
-using valuei = value<int>;
-using valueh = value<float16>;
-
-using v2f = tensor<valuef, 2>;
-using v3f = tensor<valuef, 3>;
-using v4f = tensor<valuef, 4>;
-using v2i = tensor<valuei, 2>;
-using v3i = tensor<valuei, 3>;
-using v4i = tensor<valuei, 4>;
-using m44f = metric<valuef, 4, 4>;
-using v3h = tensor<valueh, 3>;
-
-using mut_v4f = tensor<mut<valuef>, 4>;
-using mut_v3f = tensor<mut<valuef>, 3>;
-
-v3i get_coordinate(valuei id, v3i dim);
-
-using t3i = tensor<int, 3>;
-using t3f = tensor<float, 3>;
-using momentum_t = valueh;
+#include "value_alias.hpp"
 
 #define MOMENTUM_CONSTRAINT_DAMPING
 
--- code/NR4/hydrodynamics.hpp
+++ code/NR4/hydrodynamics.hpp
@@ -1,12 +1,39 @@
 #ifndef HYDRODYNAMICS_HPP_INCLUDED
 #define HYDRODYNAMICS_HPP_INCLUDED
 
+#include <toolkit/opencl.hpp>
+#include "../common/single_source.hpp"
+#include <array>
+#include "plugin.hpp"
+
 ///so. We have to sort out the classic problem, which is that
 ///we need to triple buffer the hydrodynamic state, and solve in lockstep with the classic bssn equation
 ///while also using backwards euler correctly. I'm going to copy the plugin architecture from the other project
-struct hydrodynamic_state
+struct hydrodynamic_args : adm_args_mem
 {
+    /*cl::buffer p_star;
+    cl::buffer e_star;
+    std::array<cl::buffer, 3> Si;
 
+    hydrodynamic_buffers(cl::context ctx) : p_star(ctx), e_star(ctx), Si{ctx, ctx, ctx}{}*/
+
+    buffer<valuef> p_star;
+    buffer<valuef> e_star;
+    std::array<buffer<valuef>, 3> Si;
+
+    virtual void build(value_impl::type_storage& in) override
+    {
+        using namespace value_impl::builder;
+
+        add(p_star, in);
+        add(e_star, in);
+        add(Si, in);
+    }
+
+    virtual void add_adm_S(bssn_args& args, valuef& in) override
+    {
+
+    }
 };
 
 #endif // HYDRODYNAMICS_HPP_INCLUDED
