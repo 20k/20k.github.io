2faa43cf3c100cd8f9e9fedbcfe8c049e5aac53a
20k <icedshot@gmail.com>
2025-02-12 09:51:37 +0000

    more work

--- code/NR4/hydrodynamics.cpp
+++ code/NR4/hydrodynamics.cpp
@@ -1,1 +1,48 @@
 #include "hydrodynamics.hpp"
+
+std::vector<buffer_descriptor> hydrodynamic_buffers::get_description()
+{
+    buffer_descriptor p;
+    p.name = "p*";
+
+    buffer_descriptor e;
+    e.name = "e*";
+
+    buffer_descriptor s0;
+    s0.name = "cs0";
+
+    buffer_descriptor s1;
+    s1.name = "cs1";
+
+    buffer_descriptor s2;
+    s2.name = "cs2";
+
+    return {p, e, s0, s1, s2};
+}
+
+std::vector<cl::buffer> hydrodynamic_buffers::get_buffers()
+{
+    return {p_star, e_star, Si[0], Si[1], Si[2]};
+}
+
+void hydrodynamic_buffers::allocate(cl::context ctx, cl::command_queue cqueue, t3i size)
+{
+    int64_t cells = int64_t{size.x()} * size.y() * size.z();
+
+    p_star.alloc(sizeof(cl_float) * cells);
+    e_star.alloc(sizeof(cl_float) * cells);
+
+    p_star.set_to_zero(cqueue);
+    e_star.set_to_zero(cqueue);
+
+    for(auto& i : Si)
+    {
+        i.alloc(sizeof(cl_float) * cells);
+        i.set_to_zero(cqueue);
+    }
+}
+
+buffer_provider* hydrodynamic_plugin::get_buffer_factory(cl::context ctx)
+{
+    return new hydrodynamic_buffers(ctx);
+}
--- code/NR4/hydrodynamics.hpp
+++ code/NR4/hydrodynamics.hpp
@@ -11,12 +11,6 @@
 ///while also using backwards euler correctly. I'm going to copy the plugin architecture from the other project
 struct hydrodynamic_args : adm_args_mem
 {
-    /*cl::buffer p_star;
-    cl::buffer e_star;
-    std::array<cl::buffer, 3> Si;
-
-    hydrodynamic_buffers(cl::context ctx) : p_star(ctx), e_star(ctx), Si{ctx, ctx, ctx}{}*/
-
     buffer<valuef> p_star;
     buffer<valuef> e_star;
     std::array<buffer<valuef>, 3> Si;
@@ -48,49 +42,14 @@ struct hydrodynamic_buffers : buffer_provider
 
     }
 
-    std::vector<cl::buffer> buffers;
+    virtual std::vector<buffer_descriptor> get_description() override;
+    virtual std::vector<cl::buffer> get_buffers() override;
+    virtual void allocate(cl::context ctx, cl::command_queue cqueue, t3i size) override;
+};
 
-    virtual std::vector<buffer_descriptor> get_description() override
-    {
-        buffer_descriptor p;
-        p.name = "p*";
-
-        buffer_descriptor e;
-        e.name = "e*";
-
-        buffer_descriptor s0;
-        s0.name = "cs0";
-
-        buffer_descriptor s1;
-        s1.name = "cs1";
-
-        buffer_descriptor s2;
-        s2.name = "cs2";
-
-        return {p, e, s0, s1, s2};
-    }
-
-    virtual std::vector<cl::buffer> get_buffers() override
-    {
-        return {p_star, e_star, Si[0], Si[1], Si[2]};
-    }
-
-    virtual void allocate(cl::context ctx, cl::command_queue cqueue, t3i size)
-    {
-        int64_t cells = int64_t{size.x()} * size.y() * size.z();
-
-        p_star.alloc(sizeof(cl_float) * cells);
-        e_star.alloc(sizeof(cl_float) * cells);
-
-        p_star.set_to_zero(cqueue);
-        e_star.set_to_zero(cqueue);
-
-        for(auto& i : Si)
-        {
-            i.alloc(sizeof(cl_float) * cells);
-            i.set_to_zero(cqueue);
-        }
-    }
+struct hydrodynamic_plugin : plugin
+{
+    virtual buffer_provider* get_buffer_factory(cl::context ctx) override;
 };
 
 #endif // HYDRODYNAMICS_HPP_INCLUDED
