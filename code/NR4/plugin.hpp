b778bf453e739a0c00c722b7895367bfa13de560
20k <icedshot@gmail.com>
2025-02-12 07:28:19 +0000

    starting to sketch the framework for how the plugin interface will work

--- code/NR4/plugin.hpp
+++ code/NR4/plugin.hpp
@@ -18,7 +18,25 @@ struct buffer_descriptor
     float wave_speed = 1;
 };
 
-struct buffer_set
+///hmm. What if we stick a bssn buffer pack in here? or even inherit?
+///this project is in a unique position where I don't have to be so
+///careless about passing in all-buffers due to the hideousousness of opencl
+///lets think about this. We have a bssn_buffer_pack, which contains our named bssn args
+///we also have our individual structs for each of our sim parts, eg we'll have a hydro_buffer_pack
+///so: problem one. The bssn kernel will have to accept an unlimited number of arguments
+///this is because it has to take in hydro components
+///taking in tonnes of buffers may be slower than accumulating into the default ones, except a priori for a fact
+///the #1 use case is neutron stars, when it is DEFINITELY faster not to do this due to Sij
+///for particle dynamics, it may be better to accumulate directly into sij. There's no way to do this generically, except if i use heuristics (?)
+///but eh maybe this isn't worth worrying about
+
+///decision: bssn buffers are their own thing. Nobody is going to attach new buffers to the bssn variables
+///decision: no plugin can accept adm matter variables in, I think, because its inherently ill formed. If matter mutually interacts, I'll shim it on
+///adm variables: composite of N+ buffers. So the adm buffer set will be tricky to bolt on
+
+///I need to pass in the BSSN buffers by *value*, as well as our buffer struct by *value*, or at least by pointer
+///make build a virtual fucntion, overload, and add an overloaded push
+/*struct buffer_set
 {
     std::vector<cl::buffer> bufs;
     std::vector<buffer_descriptor> desc;
@@ -33,11 +51,86 @@ struct buffer_set
 
         assert(false);
     }
-};
+
+    void append_to(cl::args& args)
+    {
+        for(auto& i : bufs)
+            args.push_back(i);
+    }
+};*/
+
 
 struct all_buffers
 {
     std::array<buffers, 3> all_bufs;
+};
+
+struct adm_args_mem : value_impl::single_source::argument_pack
+{
+    virtual void build(value_impl::type_storage& store){assert(false);}
+
+    virtual void add_adm_S(bssn_args& args, valuef& in) override{}
+    virtual void add_adm_p(bssn_args& args, valuef& in) override{}
+    virtual void add_adm_Si(bssn_args& args, tensor<valuef, 3>& in) override{}
+    virtual void add_adm_W2_Sij(bssn_args& args, tensor<valuef, 3, 3>& in) override{}
+};
+
+struct all_adm_args_mem : value_impl::single_source::argument_pack
+{
+    std::vector<adm_args_mem*> all_mem;
+
+    virtual void build(value_impl::type_storage& store) override
+    {
+        for(auto& i : all_mem)
+            i->build(store);
+    }
+
+    virtual void add_adm_S(bssn_args& args, valuef& in) override
+    {
+        for(auto& i : all_mem)
+            i->add_adm_S(args, in);
+    }
+
+    virtual void add_adm_p(bssn_args& args, valuef& in) override
+    {
+        for(auto& i : all_mem)
+            i->add_adm_p(args, in);
+    }
+
+    virtual void add_adm_Si(bssn_args& args, tensor<valuef, 3>& in) override
+    {
+        for(auto& i : all_mem)
+            i->add_adm_Si(args, in);
+    }
+
+    virtual void add_adm_W2_Sij(bssn_args& args,tensor<valuef, 3, 3>& in) override
+    {
+        for(auto& i : all_mem)
+            i->add_adm_W2_Sij(args, in);
+    }
+};
+
+struct hydrodynamic_buffers : adm_args_mem
+{
+    cl::buffer p_star;
+    cl::buffer e_star;
+    std::array<cl::buffer, 3> Si;
+
+    hydrodynamic_buffers(cl::context ctx) : p_star(ctx), e_star(ctx), Si{ctx, ctx, ctx}{}
+
+    virtual void build(value_impl::type_storage& store) override
+    {
+        using value_impl::builder;
+
+        add(p_star);
+        add(e_star);
+        add(Si);
+    }
+
+    virtual void add_adm_S(bssn_args& args, valuef& in) override
+    {
+
+    }
 };
 
 struct plugin
@@ -53,5 +146,25 @@ struct plugin
 
     virtual ~plugin(){}
 };
+
+/*struct matter_provider
+{
+    virtual value               calculate_adm_S(equation_context& ctx, standard_arguments& bssn_args) const{return 0;};
+    virtual value               calculate_adm_p(equation_context& ctx, standard_arguments& bssn_args) const{return 0;};
+    virtual tensor<value, 3, 3> calculate_adm_X_Sij(equation_context& ctx, standard_arguments& bssn_args) const{return {0,0,0,0,0,0,0,0,0};};
+    virtual tensor<value, 3>    calculate_adm_Si(equation_context& ctx, standard_arguments& bssn_args) const{return {0,0,0};};
+
+    virtual ~matter_interop(){}
+};
+
+struct matter_meta_provider : matter_provider
+{
+    std::vector<matter_provider*> all;
+
+    virtual value               calculate_adm_S(equation_context& ctx, standard_arguments& bssn_args) const override;
+    virtual value               calculate_adm_p(equation_context& ctx, standard_arguments& bssn_args) const override;
+    virtual tensor<value, 3, 3> calculate_adm_X_Sij(equation_context& ctx, standard_arguments& bssn_args) const override;
+    virtual tensor<value, 3>    calculate_adm_Si(equation_context& ctx, standard_arguments& bssn_args) const override;
+};*/
 
 #endif // PLUGIN_HPP_INCLUDED
