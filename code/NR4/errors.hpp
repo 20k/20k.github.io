6b5257249e0383af72824de5dcdec2ec095dedf7
20k <icedshot@gmail.com>
2025-02-13 09:09:29 +0000

    update momentum and hamiltonian constraints to support matter args, add plugin integration to bssn

--- code/NR4/errors.hpp
+++ code/NR4/errors.hpp
@@ -5,7 +5,7 @@
 
 void make_cG_error(cl::context ctx, int idx);
 //void make_momentum_error(cl::context ctx, int idx);
-void make_hamiltonian_error(cl::context ctx);
+//void make_hamiltonian_error(cl::context ctx);
 void make_global_sum(cl::context ctx);
 
 #endif // ERRORS_HPP_INCLUDED
