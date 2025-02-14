a04fae6ad5d886778ce330d815ee85d766e67fd8
20k <icedshot@gmail.com>
2025-02-14 06:31:58 +0000

    implement pow optimisations

--- code/common/value2.hpp
+++ code/common/value2.hpp
@@ -756,6 +756,17 @@ namespace value_impl
                 return in.args[0] + in.args[2];
         }
 
+        if(in.type == op::POW)
+        {
+            if(equivalent(in.args[1], in.args[1].make_constant_of_type(0.f)))
+                return in.args[0].make_constant_of_type(1.f);
+
+            if(equivalent(in.args[1], in.args[1].make_constant_of_type(1.f)))
+                return in.args[0];
+
+            //amd explicitly encourages you to unroll pow to loops ;_;
+        }
+
         return in;
     }
 
