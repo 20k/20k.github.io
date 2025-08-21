eac074618513bc5a73b6608662b4cec011d3cb1b
20k <icedshot@gmail.com>
2025-08-21 17:56:27 +0100

    still good

--- code/NRNext/integration.hpp
+++ code/NRNext/integration.hpp
@@ -1,6 +1,53 @@
 #ifndef INTEGRATION_HPP_INCLUDED
 #define INTEGRATION_HPP_INCLUDED
 
+template<typename T>
+inline
+T symmetric_sum(const std::vector<T>& in)
+{
+    if(in.size() == 0)
+        return T{};
+
+    if(in.size() == 1)
+        return in.front();
+
+    if(in.size() == 2)
+        return in[0] + in[1];
+
+    if((in.size() % 2) != 0)
+    {
+        int middle = (in.size() - 1) / 2;
+
+        std::vector<T> left;
+
+        for(int i=0; i < middle; i++)
+            left.push_back(in[i]);
+
+        std::vector<T> right;
+
+        for(int i=middle + 1; i < (int)in.size(); i++)
+            right.push_back(in[i]);
+
+        return (symmetric_sum(left) + symmetric_sum(right)) + in[middle];
+    }
+    else
+    {
+        int middle = in.size() / 2;
+
+        std::vector<T> left;
+
+        for(int i=0; i < middle; i++)
+            left.push_back(in[i]);
+
+        std::vector<T> right;
+
+        for(int i=middle; i < (int)in.size(); i++)
+            right.push_back(in[i]);
+
+        return symmetric_sum(left) + symmetric_sum(right);
+    }
+}
+
 ///todo: fixme args
 template<typename T, typename U>
 inline
@@ -8,18 +55,16 @@ auto integrate_1d_trapezoidal(const T& func, int n, const U& upper, const U& low
 {
     using variable_type = decltype(func(U()));
 
-    variable_type sum = 0;
+    std::vector<variable_type> sum;
 
     for(int k=1; k < n; k++)
     {
         auto coordinate = lower + k * (upper - lower) / n;
 
-        auto val = func(coordinate);
-
-        sum += val;
+        sum.push_back(func(coordinate));
     }
 
-    return ((upper - lower) / n) * (0.5f * (func(lower) + func(upper)) + sum);
+    return ((upper - lower) / n) * (0.5f * (func(lower) + func(upper)) + symmetric_sum(sum));
 }
 
 template<typename T, typename U>
